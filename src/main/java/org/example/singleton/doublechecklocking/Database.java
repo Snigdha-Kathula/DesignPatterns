858d7c139e8c8166cd0d18b2cba035f2ee979cbe
Kathula Snigdha <kathulasnigdha2k@gmail.com>
2024-02-20 07:44:27 +0530

    SingletonTheoryAdded

--- src/main/java/org/example/singleton/doublechecklocking/Database.java
+++ src/main/java/org/example/singleton/doublechecklocking/Database.java
@@ -16,7 +16,12 @@ public class Database {
         return instance;
     }
 }
-//Singleton:
-//Singleton is a Creational Design Pattern:-
-//
-//
+//Singleton:Singleton is a Creational Design Pattern:-
+// define a class that has only one instance and provides a global point of access to it.
+//Advantage of Singleton design pattern :-
+//Saves memory because object is not created at each request. Only single instance is reused again and again.
+//Usage of Singleton design pattern;-
+//Singleton pattern is mostly used in multithreaded and database applications. It is used in logging, caching, thread pools, configuration settings etc.
+// Early Initialization :- an object created at the time of class loading.
+// Lazy Initialization :- an object created at the time of request time.
+

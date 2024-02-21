8a9088e273a540b0bb9b36449cc0a5c1cd82f9c6
Kathula Snigdha <kathulasnigdha2k@gmail.com>
2024-02-21 08:27:11 +0530

    BUilderDesignPatternTheoryAdded

--- src/main/java/org/example/builder/Client.java
+++ src/main/java/org/example/builder/Client.java
@@ -2,7 +2,11 @@ package org.example.builder;
 
 public class Client {
     public static void main(String[] args) {
-        Student student = Student.getBuilder().setName("Snigdha").setAge(19).setTotalMarks(55).build();
+        Student student = Student.getBuilder().setName("sai").setAge(19).setTotalMarks(55).build();
 
     }
 }
+//Builder DesignPattern:- It's a Creation Design Pattern
+//It was used:-1) when there are lots of attributes
+//             2) when there are validations need to apply
+

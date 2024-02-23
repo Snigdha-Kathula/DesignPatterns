7299154e0c8228c66f71c4cef08a2886e53718e1
Kathula Snigdha <kathulasnigdha2k@gmail.com>
2024-02-23 09:04:39 +0530

    FactoryDefAdded

--- src/main/java/org/example/factorymethod/Client.java
+++ src/main/java/org/example/factorymethod/Client.java
@@ -7,3 +7,5 @@ public class Client {
         vehicle.start();
     }
 }
+//Factory Method is a creational design pattern that provides an interface for creating objects
+//in a superclass, but allows subclasses to alter the type of objects that will be created.

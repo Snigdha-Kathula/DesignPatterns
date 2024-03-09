c55985f37bd1bb9bd5790215c22f40a579fc640d
Kathula Snigdha <kathulasnigdha2k@gmail.com>
2024-03-09 06:11:46 +0530

    StrategyDefAdded

--- src/main/java/org/example/strategy/Client.java
+++ src/main/java/org/example/strategy/Client.java
@@ -8,3 +8,7 @@ public class Client {
         sortingContext1.sort(new int[3]);
     }
 }
+//The Strategy Design Pattern is a behavioral design pattern.
+//It allows you to dynamically change the behavior of an object by encapsulating
+//it into different strategies. This pattern enables an object to choose from multiple
+//algorithms and behaviors at runtime, rather than statically choosing a single one.
